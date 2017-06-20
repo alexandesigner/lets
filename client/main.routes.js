0c5a05c2fa5bbf48be14e2161f152a8c7a45f9e3
Allan Alexandre <alexandesigner@gmail.com>
2017-06-19 22:19:41 -0300

    Create routes and permission

--- client/main.routes.js
+++ client/main.routes.js
@@ -1,3 +1,5 @@
+import isUser from '/imports/modules/is-user';
+
 export default [
   {
     path: '/',
@@ -5,13 +7,92 @@ export default [
     component: '/imports/ui/pages/Index/Index.vue'
   },
   {
-    path: '/about',
-    name: 'about',
-    component: '/imports/ui/pages/About/About.vue'
+    path: '/documents',
+    name: 'documents',
+    component: '/imports/ui/pages/Documents/Documents.vue',
+    beforeEnter: (to, from, next) => {
+      isUser().then(response => {
+        response ? next() : next({name: 'login'})
+      })
+    }
+  },
+  {
+    path: '/documents/:id',
+    name: 'documents-details',
+    component: '/imports/ui/pages/Documents/DocumentsDetails.vue',
+    beforeEnter: (to, from, next) => {
+      isUser().then(response => {
+        response ? next() : next({name: 'login'})
+      })
+    }
   },
   {
     path: '/terms',
     name: 'terms',
     component: '/imports/ui/pages/Terms/Terms.vue'
+  },
+  {
+    path: '/login',
+    component: '/imports/ui/pages/Auth/Auth.vue',
+    redirect: { name: 'login' },
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: '/imports/ui/pages/Auth/Login.vue'
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: '/imports/ui/pages/Auth/Register.vue'
+      },
+      {
+        path: '/forgot-password',
+        name: 'forgot-password',
+        component: '/imports/ui/pages/Auth/ForgotPassword.vue'
+      },
+      {
+        path: '/new-password',
+        name: 'new-password',
+        component: '/imports/ui/pages/Auth/NewPassword.vue',
+        beforeEnter: (to, from, next) => {
+          isUser().then(response => {
+            response ? next() : next({name: 'forgot-password'})
+          })
+        }
+      }
+    ]
+  },
+  {
+    path: '/admin',
+    component: '/imports/ui/pages/Admin/Admin.vue',
+    redirect: { name: 'admin' },
+    beforeEnter: (to, from, next) => {
+      isUser().then(response => {
+        response ? next() : next({name: 'login'})
+      })
+    },
+    children: [
+      {
+        path: '/admin',
+        name: 'admin',
+        component: '/imports/ui/pages/Admin/Dashboard/Dashboard.vue'
+      },
+      {
+        path: '/admin/documents',
+        name: 'admin-documents',
+        component: '/imports/ui/pages/Admin/Documents/Documents.vue'
+      },
+      {
+        path: '/admin/documents/new',
+        name: 'admin-documents-new',
+        component: '/imports/ui/pages/Admin/Documents/DocumentsNew.vue'
+      },
+      {
+        path: '/admin/documents/:id/edit',
+        name: 'admin-documents-edit',
+        component: '/imports/ui/pages/Admin/Documents/DocumentsEdit.vue'
+      }
+    ]
   }
 ];
