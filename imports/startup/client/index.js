b2b90821ccfae6ba4abc92a819c740c6ed37203d
Allan Alexandre <alexandesigner@gmail.com>
2017-06-19 22:20:44 -0300

    Add Service worker (provisory)

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -6,7 +6,6 @@ import routerFactory from '../../routes'
 import '../../ui/stylesheets/main.styl'
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 
 // App layout
 import AppLayout from '../../ui/layout/AppLayout.vue'
@@ -14,7 +13,7 @@ import AppLayout from '../../ui/layout/AppLayout.vue'
 // App start
 Meteor.startup(() => {
 	// Start Worker
-  navigator.serviceWorker.register('/sw.js').then().catch(error => console.log(error));
+  navigator.serviceWorker.register('/sw.js').then().catch(error => console.log(error))
   // Start the router
   const router = routerFactory.create()
   new Vue({
