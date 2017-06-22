1353bf6b59f233ff1680486acb57b42b5b50b6b3
Allan Alexandre <alexandesigner@gmail.com>
2017-06-22 20:47:48 -0300

    Create simple documents
    - Before refactoring this

---
+++ imports/api/Documents/server/publications.js
@@ -0,0 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import Documents from '../documents';
+
+Meteor.publish('documents', function documents() {
+  return Documents.find({ owner: this.userId });
+});
+
+// Note: documents.view is also used when editing an existing document.
+Meteor.publish('documents.view', function documentsView(documentId) {
+  check(documentId, String);
+  return Documents.find({ _id: documentId, owner: this.userId });
+});
