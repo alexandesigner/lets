af505348c5ea3ba81b8b36e930f29f6989cabfcd
Allan Alexandre <alexandesigner@gmail.com>
2017-06-22 23:30:55 -0300

    [WIP] Add Basic mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,41 @@
+App.info({
+  name: 'Lets',
+  description: 'A minimalist boilerplate for products',
+  author: 'Allan Alexandre',
+  email: 'dev@alexandesigner.com.br',
+  website: 'https://alexandesigner.com.br/lets',
+  version: '0.1.0',
+});
+
+// App.icons({
+//   // iOS
+//   'iphone_2x': 'resources/icons/icon-60x60@2x.png',
+//   'ipad': 'resources/icons/icon-76x76.png',
+//   'ipad_2x': 'resources/icons/icon-76x76@2x.png',
+
+//   // Android
+//   'android_mdpi': 'resources/icons/icon-48x48.png',
+//   'android_hdpi': 'resources/icons/icon-72x72.png',
+//   'android_xhdpi': 'resources/icons/icon-96x96.png',
+// });
+
+// App.launchScreens({
+//   // iOS
+//   'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+//   'iphone5': 'resources/splash/splash-320x568@2x.png',
+//   'ipad_portrait': 'resources/splash/splash-768x1024.png',
+//   'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+//   'ipad_landscape': 'resources/splash/splash-1024x768.png',
+//   'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+
+//   // Android
+//   'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+//   'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+//   'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+//   'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+//   'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+//   'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png',
+// });
+
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#674CD8');
