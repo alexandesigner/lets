df105ec1186a7f967744cd26e0c1ff9e5d1b5873
Allan Alexandre <alexandesigner@gmail.com>
2017-06-22 21:04:01 -0300

    [WIP] Api user methods

--- imports/api/Users/methods.js
+++ imports/api/Users/methods.js
@@ -3,47 +3,11 @@ import { check } from 'meteor/check'
 import { SimpleSchema } from 'meteor/aldeed:simple-schema'
 import { ValidatedMethod } from 'meteor/mdg:validated-method'
 
-import { UsersCollection } from './users'
-
-// Extend profile object field in UsersCollection
+// Extend profile object field in Meteor Users
 export const ProfileSchema = new SimpleSchema({
   firstName: { type: String, },
   lastName: { type: String, },
-  phone: { type: String, },
-  cpf: { type: String, optional: true, },
-  cnpj: { type: String, optional: true, }
-})
-
-// Insert User
-export const insertUsers = new ValidatedMethod({
-  name: 'Users.methods.insert',
-  validate: new SimpleSchema({
-    createdAt: { type: Date, optional: true, },
-    emails: { type: Array, optional: true, },
-      'emails.$': { type: Object, },
-      'emails.$.address': { type: String, regEx: SimpleSchema.RegEx.Email, },
-      'emails.$.verified': { type: Boolean, },
-    profile: { type: ProfileSchema, optional: true, },
-    services: { type: Object, optional: true, blackbox: true, },
-    roles: { type: Object, optional: true, blackbox: true, },
-    locations: { type: Array, optional: true, },
-      'locations.$': { type: Object, },
-      'locations.$.street': { type: String },
-      'locations.$.number': { type: String, },
-      'locations.$.complement': { type: String, },
-      'locations.$.neighborhood': { type: String, },
-      'locations.$.city': { type: String, },
-      'locations.$.state': { type: String, },
-      'locations.$.reference': { type: String, }
-
-  }).validator(),
-  run(user) {
-    try {
-      return UsersCollection.insert(user)
-    } catch (exception) {
-      throw new Meteor.Error('500', exception.message)
-    }
-  }
+  phone: { type: String, }
 })
 
 // Remove User
@@ -54,7 +18,7 @@ export const removeUser = new ValidatedMethod({
   }).validator(),
   run(id) {
     try {
-      UsersCollection.remove(id)
+      Meteor.users.remove(id)
     } catch (exception) {
       throw new Meteor.Error('500', exception.message)
     }
@@ -71,6 +35,7 @@ export const updateUser = new ValidatedMethod({
       'emails.$': { type: Object, },
       'emails.$.address': { type: String, regEx: SimpleSchema.RegEx.Email, },
       'emails.$.verified': { type: Boolean, },
+    password: { type: String },
     profile: { type: ProfileSchema, optional: true, },
     services: { type: Object, optional: true, blackbox: true, },
     roles: { type: Object, optional: true, blackbox: true, },
@@ -86,7 +51,7 @@ export const updateUser = new ValidatedMethod({
   }).validator(),
   run(user) {
     try {
-      UsersCollection.update(user._id, {
+      Meteor.users.update(user._id, {
         $set: {
           updatedAt: new Date(),
           password: user.services.password,
@@ -97,17 +62,16 @@ export const updateUser = new ValidatedMethod({
           profile: {
             firstName: user.profile.firstName,
     				lastName: user.profile.lastName,
-    				phone: user.profile.phone,
-    				cpf: user.profile.cpf,
-    				cnpj: user.profile.cnpj
+    				phone: user.profile.phone
           },
+          roles: user.roles,
           locations: [{
             address: {
               street: values.street,
               number: values.number,
               complement: values.complement,
               city: values.city,
-              state: values.state,
+              state: values.state
             }
           }]
         }
