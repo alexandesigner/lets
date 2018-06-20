471c6ea77ca7c959f9f6517b0cb45d1ea6b2fe6f
Allan Alexandre <alexandesigner@gmail.com>
2018-06-20 17:32:20 -0300

    Initial image upload

--- boilerplate/src/imports/ui/pages/Admin/Documents/DocumentsEdit.vue
+++ boilerplate/src/imports/ui/pages/Admin/Documents/DocumentsEdit.vue
@@ -11,6 +11,22 @@
           <el-form :model="editDocument" :rules="rules" ref="editDocument" class="form-model">
             <el-row :gutter="10">
               <el-col :lg="24">
+                  <el-upload
+                    class="avatar-uploader"
+                    action="http://localhost:3000/"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload">
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                  </el-upload>
+              </el-col>
+              <el-col :lg="24">
+                <el-form-item prop="owner">
+                  <el-input size="large" v-model="editDocument.owner" auto-complete="off" :disabled="true"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :lg="24">
                 <el-form-item prop="title">
                   <el-input placeholder="Please input document title" size="large" v-model="editDocument.title" auto-complete="off"></el-input>
                 </el-form-item>
@@ -41,7 +57,10 @@
     data: () => ({
       documents: [],
       users: [],
+      imageUrl: '',
+      imgBase64: '',
       editDocument: {
+        owner: '',
         title: '',
         body: ''
       },
@@ -54,16 +73,18 @@
         ]
       },
     }),
-    async mounted () {
-      this.$nextTick(() => {
-        this.renderData()
-      })
+    watch: {
+      'documents': function(doc) {
+        this.$nextTick(function() {
+          this.editDocument = {
+            owner: doc[0].owner,
+            title: doc[0].title,
+            body: doc[0].body
+          }
+        })
+      }
     },
     methods: {
-      renderData () {
-        this.editDocument.title = this.documents[0].title
-        this.editDocument.body = this.documents[0].body
-      },
       handleSubmitForm (formName) {
         this.$refs[formName].validate((valid) => {
           try {
@@ -92,11 +113,31 @@
             })
           }
         })
+      },
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw)
+        let reader = new FileReader()
+        reader.readAsDataURL(file.raw)
+        reader.onload = () => {
+          this.imgBase64 = reader.result
+        }
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg'
+        const isLt2M = file.size / 1024 / 1024 < 2
+
+        if (!isJPG) {
+          this.$message.error('Avatar picture must be JPG format!')
+        }
+        if (!isLt2M) {
+          this.$message.error('Avatar picture size can not exceed 2MB!')
+        }
+        return isJPG && isLt2M
       }
     },
     meteor: {
       $subscribe: {
-        'documents.owner': [],
+        'documents.owner': []
       },
       users() {
         return Meteor.users.find({})
@@ -114,3 +155,30 @@
     }
   }
 </script>
+
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    float: left;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
