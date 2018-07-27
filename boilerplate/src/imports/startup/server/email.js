3b37bf2a3e8e66c0a3668214e933f4637cc8a369
Allan Alexandre <alexandesigner@gmail.com>
2018-07-27 14:22:44 -0300

    Lint file

--- boilerplate/src/imports/startup/server/email.js
+++ boilerplate/src/imports/startup/server/email.js
@@ -4,7 +4,6 @@ if (Meteor.isDevelopment) {
   if (Meteor.settings.private && Meteor.settings.private.MAIL_URL) {
     process.env.MAIL_URL = Meteor.settings.private.MAIL_URL
   } else {
-    console.warn('[Lets] - Email settings are not configured. Emails will not be sent. ');
+    console.warn('[Lets] - Email settings are not configured. Emails will not be sent. ')
   }
 }
-
